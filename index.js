ea8b420e2375c34fb0e20aaabf773d4466dc0fdd
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-15 22:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,3 @@ var recipes = {
   'Coconut Oil':'1/2 Cup',
   'Almond Flour':'1 Cup'
   }
-
-
-  function updateObjectWithKeyAndValue(object, key, value) {
-    object.key = 'value'
-    return object
-  }
-
-  updateObjectWithKeyAndValue(recipes, salt, '1 Tsp')
-
-  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
-  }
