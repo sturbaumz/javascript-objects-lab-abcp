74bb47c0e1a69fabebc3208d549317e18d3a93cb
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-15 23:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object.key = value
+  return object.key = 'value'
 }
